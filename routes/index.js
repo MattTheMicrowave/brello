1ed84824318c3455ab4ab3baf252bff848f3413c
MattTheMicrowave <mwlluis@hotmail.com>
2016-05-13 17:11:00 -0500

    lesson 03

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var TaskModel = require('../models/TaskModel.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  TaskModel.find(function(err, tasks){
+    res.render('index', {
+      title: 'Express',
+      tasks: tasks
+    });
+});
+});
+
+/* POST Create a task. */
+router.post('/tasks', function(req, res, next) {
+tasks.push(req.body.task);
+  res.redirect('/');
 });
 
 module.exports = router;
